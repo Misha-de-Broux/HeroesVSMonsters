6c9b6f771f53e41b1206aa08ab0beac6300961ea
Misha de Broux <misha.debroux@gmail.com>
2024-03-29 09:38:00 +0100

    creation de la classe et des attributs

---
+++ Models/Weapon.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Models {
+    public class Weapon {
+        private Die _die;
+        private int _modifier;
+        public int DamageDie {  get; init; }
+        public string Name { get; init; }
+        public bool IsSecondary { get; init; }
+        public bool IsTwoHanded { get; init; }
+        
+    }
+}
